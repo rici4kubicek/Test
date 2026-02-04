551961f06e7e64f807cf0148166a926ad79325c0
Richard Kubíček <rici.kubicek@gmail.com>
2026-02-04 19:26:11 +0000

    Periodic lambda invocation

--- app/stack.py
+++ app/stack.py
@@ -1,5 +1,7 @@
 import aws_cdk as cdk
 from aws_cdk import aws_lambda as lambda_
+from aws_cdk import aws_events as events
+from aws_cdk import aws_events_targets as targets
 import os
 
 
@@ -14,10 +16,28 @@ class LambdaStack(cdk.Stack):
             runtime=lambda_.Runtime.PYTHON_3_12,
             handler="index.handler",
             code=lambda_.Code.from_asset(os.path.join(os.path.dirname(__file__), "../lambda")),
-            description="A simple Lambda function",
+            description="Lambda function to fetch EUR/CZK and USD/CZK exchange rates from CNB",
             timeout=cdk.Duration.seconds(30),
             memory_size=128,
         )
+
+        # Create EventBridge rule to trigger Lambda daily at 16:00 UTC
+        # Cron expression: 0 16 * * ? *  (16:00 UTC every day)
+        rule = events.Rule(
+            self,
+            "DailyExchangeRateRule",
+            schedule=events.Schedule.cron(
+                hour="16",
+                minute="0",
+                day="*",
+                month="*",
+                week_day="*",
+            ),
+            description="Trigger exchange rate Lambda function daily at 16:00 UTC",
+        )
+
+        # Add Lambda function as target
+        rule.add_target(targets.LambdaFunction(lambda_function))
 
         # Output the function name
         cdk.CfnOutput(
@@ -26,4 +46,13 @@ class LambdaStack(cdk.Stack):
             value=lambda_function.function_name,
             description="Name of the Lambda function",
             export_name="LambdaFunctionName",
+        )
+
+        # Output the EventBridge rule
+        cdk.CfnOutput(
+            self,
+            "EventBridgeRuleName",
+            value=rule.rule_name,
+            description="Name of the EventBridge rule",
+            export_name="EventBridgeRuleName",
         )
